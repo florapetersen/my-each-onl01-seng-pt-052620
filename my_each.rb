7f837bd25fb51e72a38b277cc72a533fabb70ed4
Flora Petersen <floramarina@comcast.net>
2020-06-05 19:00:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,13 @@
 def my_each(array)
-  i = 0
-  while i < array.length
+  counter = 0
+  while counter < array.length
     yield
-    i += 1
+    counter += 1
   end
 end
 
 collection = [1, 2, 3, 4]
+
 my_each(collection) do |i|
   puts i
 end
